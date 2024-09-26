508e10b67d7d6270f0119dc2d1f7714d46391b63
Samuel Ting li Wei <samueltlw17@gmail.com>
2024-09-27 01:16:04 +0800

    update projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,37 +8,40 @@ const projectData = [
         title: "Game Brochure",
         description: "This is a project to design a brochure for a product using Adobe Photoshop.",
         image: "/projects/Picture1.jpg",
+        gitUrl: "https://drive.google.com/drive/folders/1OYN4WOVZtRZHO23mYMtdgBeaY73__-DA?usp=drive_link"
     },
     {
         id: 2,
         title: "NumberNest",
         description: "This is a project of creating an application for kids-learning using Adobe Animate.",
         image: "/projects/Picture2.png",
+        gitUrl: "https://drive.google.com/drive/folders/1xefY3TLWoE5I9Ai0uii55ySBtVU2Xrkc?usp=drive_link"
     },
     {
         id: 3,
         title: "Character Modelling",
         description: "This is a project to create a 3d character using Autodesk 3ds Max.",
         image: "/projects/InProgress.jpg",
+        gitUrl: "#"
     },
     {
         id: 4,
         title: "VR Chemical Simulator",
         description: "This is a project to create a simulator in VR environment using Unity.",
         image: "/projects/InProgress.jpg",
-        gitUrl: "https://github.com/megamask"
+        gitUrl: "#"
     }, {
         id: 5,
         title: "E-portfolio",
         description: "This is a project to create an e-portfolio using Adobe Animate.",
         image: "/projects/InProgress.jpg",
-        gitUrl: "https://github.com/trlau/zkJ0M"
+        gitUrl: "#"
     }, {
         id: 6,
         title: "Plant Learning",
         description: "This is a project to create a website for posting blog and learn planting using HTML and C#.",
         image: "/projects/InProgress.jpg",
-        gitUrl: "https://github.com/0xBenjamintan/attestMe"
+        gitUrl: "#"
     }, 
 
 ];
